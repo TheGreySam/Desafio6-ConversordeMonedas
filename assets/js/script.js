e91734a0b460238771dd2a20b40b05935a3187d0
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2022-12-01 15:59:44 -0300

    commit 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-async function getCoins(){
+async function getCoins() {
     try {
         const res = await fetch("https://mindicador.cl/api")
         const data = await res.json()
@@ -7,15 +7,14 @@ async function getCoins(){
 
         let CoinSelector = ''
         CoinSelector += `
-        <option selected>Seleccione moneda</option>
+        <option selected id="selected">Seleccione moneda</option>
                     <option value="${data.dolar.valor}">${data.dolar.nombre}</option>
                     <option value="${data.euro.valor}">${data.euro.nombre}</option>
                     <option value="${data.bitcoin.valor}">${data.bitcoin.nombre}</option>
-                    
-        ` 
-         const selectCoin =
-         document.querySelector(".CoinSelector")
-         selectCoin.innerHTML = CoinSelector
+        `
+        const selectCoin =
+            document.querySelector(".CoinSelector")
+        selectCoin.innerHTML = CoinSelector
     } catch (e) {
         alert(e.message);
     }
@@ -28,17 +27,41 @@ getCoins()
 function Calcular() {
     var inputNumber = document.getElementById("inputNumber").value
     var selectedItem = document.getElementById("Coins").value
+    var selectedItemName = document.getElementById("selected").innerText
     console.log(inputNumber)
     console.log(selectedItem)
-    
+    console.log(selectedItemName)
+
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: 'numero',
+                data: [19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 
     var total = (inputNumber / selectedItem).toFixed(2)
 
     let CoinResult = ''
-        CoinResult += `
-        <h5 class="card-text m-3">Resultado: $ ${total}</h5>
-        ` 
-         const CoinOP =
-         document.querySelector(".CoinResult")
-         CoinOP.innerHTML = CoinResult
+    CoinResult += `
+        <h5 class="card-text m-3">Resultado: $ ${total} ${selectedItemName}</h5>
+        
+        `
+    const CoinOP =
+        document.querySelector(".CoinResult")
+    CoinOP.innerHTML = CoinResult
 }
+
