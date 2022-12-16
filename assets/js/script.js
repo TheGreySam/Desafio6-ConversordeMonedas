4f8d454a949010d9d080a99b69547273e823d668
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2022-12-16 13:10:12 -0300

    commit 8 grafico reparado

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,39 @@ async function getCoins() {
         CoinResult.innerHTML = 
         `<h5 class="card-text m-3">Resultado: </h5>
         <h3 class="m-3">$ ${inputNumber.value / data[selectedItem.value].valor} ${data[selectedItem.value].nombre}<h3>`;
-       
+
+        const resB = await fetch(`https://mindicador.cl/api/${data[selectedItem.value].codigo}`);
+        const dataB = await resB.json();
+        let labelsData = [];
+        let dataPrice = [];
+        dataB.serie.forEach(element => {
+            let nuevafecha = element.fecha.split("T")[0];
+            element.fecha = nuevafecha;
+            labelsData.push(element.fecha);
+            dataPrice.push(element.valor);
+        });
+        console.log(dataB)
+        console.log(labelsData, dataPrice)
+
+        new Chart(document.getElementById("line-chart"), {
+            type: 'line',
+            data: {
+              labels: labelsData.reverse(),
+              datasets: [{ 
+                  data: dataPrice.reverse(),
+                  label: `${dataB.nombre} - Historial`,
+                  borderColor: "#3e95cd",
+                  fill: true
+                }
+              ]
+            },
+            options: {
+              title: {
+                display: false,
+                text: 'World population per region (in millions)'
+              }
+            }
+          });
     } catch (e) {
         alert(e.message);
     }
